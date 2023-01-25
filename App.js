4287536f3b0d6f2a6aabedd5cf5c3c3dc4ab198c
hassancodess <dev.hassancodes@gmail.com>
2023-01-25 17:04:37 +0500

    cleanup

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// Navigation
+import MainNavigator from './navigation/MainNavigator';
+
+const App = () => {
+  return (
+    <>
+      <MainNavigator />;
+    </>
+  );
+};
+
+export default App;
