e667a8b55dd61f3131996dda7517404c369166e6
hassancodess <dev.hassancodes@gmail.com>
2023-03-12 23:02:02 +0500

    remaining

--- src/screens/AlbumStack/Photo.js
+++ src/screens/AlbumStack/Photo.js
@@ -1,21 +1,34 @@
-import {Text, View, Image} from 'react-native';
+import {Text, View, Image, Pressable} from 'react-native';
 import React from 'react';
 import FastImage from 'react-native-fast-image';
 import Container from '../../components/UI/Container';
+import Exif from 'react-native-exif';
+import {useEffect} from 'react';
 
 const Photo = ({route, navigation}) => {
   const {item} = route.params;
+
+  // useEffect(() => {
+  const handleExif = () => {
+    // Exif.getExif(item.path)
+    //   .then(msg => console.warn('OK: ' + JSON.stringify(msg)))
+    //   .catch(msg => console.warn('ERROR: ' + msg));
+  };
+  // }, []);
+
   console.log(item);
   return (
     // <Container>
-    <View className="h-full w-full justify-center">
-      <FastImage
-        // style={styles.image}
-        className="h-5/6 w-full"
-        source={item.path}
-        resizeMode={FastImage.resizeMode.contain}
-      />
-    </View>
+    <Pressable onPress={handleExif}>
+      <View className="h-full w-full justify-center">
+        <FastImage
+          // style={styles.image}
+          className="h-5/6 w-full"
+          source={item.path}
+          resizeMode={FastImage.resizeMode.contain}
+        />
+      </View>
+    </Pressable>
     // </Container>
   );
 };
