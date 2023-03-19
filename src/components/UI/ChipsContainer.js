c5d2fc6bf65d2ee7190e5362a69b13cc4419f23a
hassancodess <dev.hassancodes@gmail.com>
2023-03-19 20:17:25 +0500

    finish Edit Details

---
+++ src/components/UI/ChipsContainer.js
@@ -0,0 +1,77 @@
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, FlatList, Pressable} from 'react-native';
+import {
+  Modal,
+  Portal,
+  TextInput,
+  Chip,
+  Button,
+  Menu,
+  Divider,
+} from 'react-native-paper';
+import GlobalStyles from '../../utils/GlobalStyles';
+const ChipsContainer = ({
+  items,
+  setItems,
+  itemName,
+  setItemName,
+  itemID,
+  setItemID,
+  isEditing,
+  setIsEditing,
+  clearStates,
+}) => {
+  const [visible, setVisible] = useState(null);
+  const openMenu = () => setVisible(null);
+  const closeMenu = () => setVisible(null);
+
+  const handleEditItem = id => {
+    const item = items.find(item => item.id == id);
+    setIsEditing(true);
+    setItemName(item.name);
+    setItemID(id);
+  };
+  const handleDeleteItem = id => {
+    const updatedItem = items.filter(item => item.id !== id);
+    setItems(updatedItem);
+    clearStates();
+  };
+
+  const handleChipEdit = id => {
+    handleEditItem(id);
+    closeMenu();
+  };
+  const handleChipDelete = id => {
+    handleDeleteItem(id);
+  };
+
+  return (
+    <View style={styles.chipContainer}>
+      {items.map((item, index) => (
+        <Menu
+          key={index}
+          visible={visible == item.id}
+          onDismiss={closeMenu}
+          anchor={
+            <Chip mode="flat" onPress={() => setVisible(item.id)}>
+              {item.name}
+            </Chip>
+          }>
+          <Menu.Item onPress={() => handleChipEdit(item.id)} title="Edit" />
+          <Divider />
+          <Menu.Item onPress={() => handleChipDelete(item.id)} title="Delete" />
+        </Menu>
+      ))}
+    </View>
+  );
+};
+
+export default ChipsContainer;
+
+const styles = StyleSheet.create({
+  chipContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    gap: 10,
+  },
+});
--- src/screens/PhotoStack/EditDetails.js
+++ src/screens/PhotoStack/EditDetails.js
@@ -1,41 +1,78 @@
 import React, {useState} from 'react';
-import {StyleSheet, Text, View, FlatList, Pressable} from 'react-native';
-import {
-  Modal,
-  Portal,
-  TextInput,
-  Chip,
-  Button,
-  Menu,
-  Divider,
-} from 'react-native-paper';
+import {StyleSheet, Text, View} from 'react-native';
+import {TextInput, Chip, Button} from 'react-native-paper';
 import GlobalStyles from '../../utils/GlobalStyles';
+import ChipsContainer from '../../components/UI/ChipsContainer';
 
 const EditDetails = () => {
+  const [location, setLocation] = useState();
+  const [isPersonEditing, setIsPersonEditing] = useState();
+  const [personID, setPersonID] = useState();
   const [person, setPerson] = useState();
   const [people, setPeople] = useState([
     {id: 1, name: 'Alesha'},
     {id: 2, name: 'Hassan'},
     {id: 3, name: 'Amna'},
   ]);
-  const [visible, setVisible] = useState(null);
-  const openMenu = () => setVisible(null);
-  const closeMenu = () => setVisible(null);
-  // const showModal = () => setVisible(true);
-  // const hideModal = () => setVisible(false);
-  // const containerStyle = {backgroundColor: 'white', padding: 20, margin: 60};
-
+  const [isEventEditing, setIsEventEditing] = useState();
+  const [eventID, setEventID] = useState();
+  const [event, setEvent] = useState();
+  const [events, setEvents] = useState([
+    {id: 1, name: 'Spring Gala 23'},
+    {id: 2, name: `Hassan's Birthday`},
+    {id: 3, name: 'NasCon 23'},
+  ]);
   const handleAddPerson = () => {
-    setPeople([
-      ...people,
-      {id: Math.floor(Math.random() * 100) + 1, name: person},
-    ]);
-    setPerson('');
+    if (!isPersonEditing) {
+      setPeople([
+        ...people,
+        {id: Math.floor(Math.random() * 100) + 1, name: person},
+      ]);
+    } else {
+      const updatedPeople = people.map(item => {
+        if (item.id === personID) {
+          return {...item, name: person};
+        }
+        return item;
+      });
+      setPeople(updatedPeople);
+    }
+    clearPersonEditingStates();
   };
+
+  const handleAddEvent = () => {
+    if (!isEventEditing) {
+      setEvents([
+        ...events,
+        {id: Math.floor(Math.random() * 100) + 1, name: event},
+      ]);
+    } else {
+      const updatedEvents = events.map(item => {
+        if (item.id === eventID) {
+          return {...item, name: event};
+        }
+        return item;
+      });
+      setEvents(updatedEvents);
+    }
+    clearEventsEditingStates();
+  };
+
+  const clearPersonEditingStates = () => {
+    setPerson('');
+    setPersonID('');
+    setIsPersonEditing(false);
+  };
+  const clearEventsEditingStates = () => {
+    setEvent('');
+    setEventID('');
+    setIsEventEditing(false);
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.flexContainer}>
-        {/* Row Item */}
+        {/* People */}
         <View style={styles.rowContainer}>
           <Text style={styles.title}>People</Text>
           <TextInput
@@ -43,28 +80,58 @@ const EditDetails = () => {
             onChangeText={text => setPerson(text)}
             style={styles.input}
             onSubmitEditing={handleAddPerson}
-            placeholder="type something..."
+            placeholder="Type a person's name"
           />
-          <View style={{flexDirection: 'row', flexWrap: 'wrap', gap: 10}}>
-            {people.map((item, index) => (
-              <Menu
-                key={index}
-                visible={visible == item.id}
-                onDismiss={closeMenu}
-                anchor={
-                  <Chip mode="flat" onPress={() => setVisible(item.id)}>
-                    {item.name}
-                  </Chip>
-                }>
-                <Menu.Item onPress={() => {}} title="Edit" />
-                <Divider />
-                <Menu.Item onPress={() => {}} title="Delete" />
-              </Menu>
-            ))}
-          </View>
-          {/* </View> */}
+          <ChipsContainer
+            items={people}
+            setItems={setPeople}
+            itemID={personID}
+            setItemID={setPersonID}
+            itemName={person}
+            setItemName={setPerson}
+            isEditing={isPersonEditing}
+            setIsEditing={setIsPersonEditing}
+            clearStates={clearPersonEditingStates}
+          />
         </View>
-        {/* Row Item */}
+        {/* Events */}
+        <View style={styles.rowContainer}>
+          <Text style={styles.title}>Events</Text>
+          <TextInput
+            value={event}
+            onChangeText={text => setEvent(text)}
+            style={styles.input}
+            onSubmitEditing={handleAddEvent}
+            placeholder="Type an event"
+          />
+          <ChipsContainer
+            items={events}
+            setItems={setEvents}
+            itemID={eventID}
+            setItemID={setEventID}
+            itemName={event}
+            setItemName={setEvent}
+            isEditing={isEventEditing}
+            setIsEditing={setIsEventEditing}
+            clearStates={clearEventsEditingStates}
+          />
+        </View>
+        {/* Location */}
+        <View style={styles.rowContainer}>
+          <Text style={styles.title}>Location</Text>
+          <TextInput
+            value={location}
+            onChangeText={text => setLocation(text)}
+            style={styles.input}
+            placeholder="Enter a location"
+            right={<TextInput.Icon icon="map-marker-radius" />}
+          />
+          <View style={{flexDirection: 'row'}}>
+            <Chip mode="flat">{location}</Chip>
+          </View>
+        </View>
+        {/* Save Button */}
+        <Button mode="contained">Save</Button>
       </View>
     </View>
   );
@@ -91,7 +158,7 @@ const styles = StyleSheet.create({
   },
   chipContainer: {
     flexDirection: 'row',
-    backgroundColor: 'red',
+    flexWrap: 'wrap',
     gap: 10,
   },
 });
