a859a74e4189b3ee5355980e4312dd064463488f
hassancodess <dev.hassancodes@gmail.com>
2023-03-19 23:00:10 +0500

    cleanup

--- src/utils/CameraRoll.js
+++ src/utils/CameraRoll.js
@@ -1,5 +1,4 @@
 import {Platform, PermissionsAndroid} from 'react-native';
-import {PERMISSIONS, request, RESULTS} from 'react-native-permissions';
 import {CameraRoll} from '@react-native-camera-roll/camera-roll';
 
 async function hasAndroidPermission() {
@@ -58,23 +57,6 @@ export const getImagesInGroups = async () => {
 };
 
 export const getAlbums = async () => {
-  // const permission =
-  //   Platform.OS === 'ios'
-  //     ? PERMISSIONS.IOS.PHOTO_LIBRARY
-  //     : PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE;
-
-  // let permissionResult = await request(permission);
-
-  // if (permissionResult !== RESULTS.GRANTED) return;
-
-  // if (Platform.OS === 'android' && Platform.Version >= 29) {
-  //   permissionResult = await request(PERMISSIONS.ANDROID.ACCESS_MEDIA_LOCATION);
-  //   if (permissionResult !== RESULTS.GRANTED) return;
-  // }
-
-  // if (Platform.OS === 'android') {
-  //   const r = await PermissionsAndroid.request(PERMISSIONS.ANDROID.CAMERA);
-  // }
   if (Platform.OS === 'android' && !(await hasAndroidPermission())) {
     console.log('returned');
     return;
