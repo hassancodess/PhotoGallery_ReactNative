44b766a95d07eb58b2baf006486a5072fce1bc66
hassancodess <dev.hassancodes@gmail.com>
2023-03-20 16:25:12 +0500

    add Utility Functions

--- App.js
+++ App.js
@@ -1,8 +1,46 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 // Navigation
 import MainStackNavigator from './src/navigation/MainStack/MainStackNavigator';
+// Database
+import {
+  createAlbumTable,
+  createPhotoTable,
+  createPersonTable,
+  createEventTable,
+  createAlbumPhotoTable,
+  createPhotoPersonTable,
+  createPhotoEventTable,
+  openDBConnection,
+  deleteTables,
+  fetchPhotos,
+} from './src/database/PhotoDB';
 
+const InitialSetup = async () => {
+  await openDBConnection();
+  await createAlbumTable();
+  await createPhotoTable();
+  await createPersonTable();
+  await createEventTable();
+  await createAlbumPhotoTable();
+  await createPhotoPersonTable();
+  await createPhotoEventTable();
+};
+
+const clearDatabase = async () => {
+  await openDBConnection();
+  await deleteTables();
+};
+const getPhotos = async () => {
+  await openDBConnection();
+  const res = await fetchPhotos();
+  console.log('Res', res);
+};
 const App = () => {
+  useEffect(() => {
+    InitialSetup();
+    // getPhotos();
+    // clearDatabase();
+  }, []);
   return (
     <>
       <MainStackNavigator />
