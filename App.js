939a32be19fae773edf0dff7c0901dfd79c9b57c
hassancodess <dev.hassancodes@gmail.com>
2023-04-04 08:27:25 -0700

    cleanup

--- App.js
+++ App.js
@@ -1,64 +1,25 @@
 import React, {useLayoutEffect} from 'react';
 // Navigation
 import MainStackNavigator from './src/navigation/MainStack/MainStackNavigator';
-// Database
+// Database Operations
 import {
-  createAlbumTable,
-  createPhotoTable,
-  createPersonTable,
-  createEventTable,
-  createAlbumPhotoTable,
-  createPhotoPersonTable,
-  createPhotoEventTable,
-  openDBConnection,
-  deleteTables,
-  fetchPhotos,
-  fetchAlbums,
-  addAlbum,
-} from './src/database/PhotoDB';
-import {getImages} from './src/utils/CameraRoll';
+  createTables,
+  clearDatabase,
+  getPhotos,
+  getAlbums,
+  createAlbum,
+} from './src/database/utils';
 
 const InitialSetup = async () => {
-  await openDBConnection();
-  await createAlbumTable();
-  await createPhotoTable();
-  await createPersonTable();
-  await createEventTable();
-  await createAlbumPhotoTable();
-  await createPhotoPersonTable();
-  await createPhotoEventTable();
+  await createTables();
+  await createAlbum();
+  const albums = await getAlbums();
+  const photos = await getPhotos();
 };
 
-const clearDatabase = async () => {
-  await openDBConnection();
-  await deleteTables();
-};
-const getPhotos = async () => {
-  await openDBConnection();
-  const res = await fetchPhotos();
-  console.log('Res', res);
-};
-const getAlbums = async () => {
-  await openDBConnection();
-  const res = await fetchAlbums();
-  // console.log('Res', res);
-};
-const createAlbum = async () => {
-  await openDBConnection();
-  const albums = await fetchAlbums();
-  const image = await getImages(1);
-  if (albums.length < 1) {
-    addAlbum('Others', image[0].image.uri);
-  } else {
-    console.log('Others Album Already Created');
-  }
-};
 const App = () => {
   useLayoutEffect(() => {
-    // InitialSetup();
-    // createAlbum();
-    // getAlbums();
-    // getPhotos();
+    InitialSetup();
     // clearDatabase();
   }, []);
   return (
