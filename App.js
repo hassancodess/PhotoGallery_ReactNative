d539228de75c692113e6c80435b9089f17bb052e
hassancodess <dev.hassancodes@gmail.com>
2023-04-28 04:02:04 +0500

    some cleanup

--- App.js
+++ App.js
@@ -1,27 +1,8 @@
-import React, {useLayoutEffect} from 'react';
+import React from 'react';
 // Navigation
 import MainStackNavigator from './src/navigation/MainStack/MainStackNavigator';
-// Database Operations
-import {
-  createTables,
-  clearDatabase,
-  getPhotos,
-  getAlbums,
-  createAlbum,
-} from './src/database/utils';
-
-const InitialSetup = async () => {
-  await createTables();
-  await createAlbum();
-  const albums = await getAlbums();
-  const photos = await getPhotos();
-};
 
 const App = () => {
-  // useLayoutEffect(() => {
-  //   InitialSetup();
-  //   // clearDatabase();
-  // }, []);
   return (
     <>
       <MainStackNavigator />
