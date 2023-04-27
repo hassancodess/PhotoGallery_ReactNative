b62a6b4fca5ba3bf37f9aca7725c3f3c65808c24
hassancodess <dev.hassancodes@gmail.com>
2023-04-28 04:02:47 +0500

    works

--- src/screens/HomeTabs/Map.js
+++ src/screens/HomeTabs/Map.js
@@ -1,14 +1,40 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import MapView from 'react-native-maps';
 
 const Map = () => {
   return (
-    <View>
-      <Text>Map</Text>
+    <View style={styles.container}>
+      {/* <MapView
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      /> */}
+      <MapView
+        style={{flex: 1}}
+        //  provider={PROVIDER_GOOGLE}
+        // showsUserLocation
+        initialRegion={{
+          latitude: 33.5651,
+          longitude: 73.0169,
+          latitudeDelta: 0.09,
+          longitudeDelta: 0.05,
+        }}
+      />
     </View>
   );
 };
 
 export default Map;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingVertical: 10,
+    paddingHorizontal: 5,
+    backgroundColor: 'green',
+  },
+});
