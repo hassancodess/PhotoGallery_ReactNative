7266ec31455a7c878c25ca5f6a7c3c425e35d3b7
hassancodess <dev.hassancodes@gmail.com>
2023-04-28 05:15:16 +0500

    feat:add map & change marker

--- src/screens/HomeTabs/Map.js
+++ src/screens/HomeTabs/Map.js
@@ -1,29 +1,65 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
-import MapView from 'react-native-maps';
+import React, {useState, useEffect, useLayoutEffect} from 'react';
+import MapView, {Marker} from 'react-native-maps';
+import GetLocation from 'react-native-get-location';
 
 const Map = () => {
+  const [location, setLocation] = useState({
+    latitudeDelta: 0.002,
+    longitudeDelta: 0.002,
+  });
+  const getCurrentLocation = () => {
+    GetLocation.getCurrentPosition({
+      enableHighAccuracy: true,
+      timeout: 60000,
+    })
+      .then(location => {
+        // console.log(location);
+        setLocation({
+          latitude: location.latitude,
+          longitude: location.longitude,
+          latitudeDelta: 0.002,
+          longitudeDelta: 0.002,
+        });
+      })
+      .catch(error => {
+        const {code, message} = error;
+        console.warn(code, message);
+      });
+  };
+  const handleRegionChange = region => {
+    setLocation({
+      ...location,
+      latitude: region.latitude,
+      longitude: region.longitude,
+    });
+  };
+  const handleMarkerChange = event => {
+    // console.log('HEHE', event.nativeEvent.coordinate);
+    const region = event.nativeEvent.coordinate;
+    setLocation({
+      ...location,
+      latitude: region.latitude,
+      longitude: region.longitude,
+    });
+  };
+  useLayoutEffect(() => {
+    getCurrentLocation();
+  }, []);
   return (
     <View style={styles.container}>
-      {/* <MapView
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      /> */}
-      <MapView
-        style={{flex: 1}}
-        //  provider={PROVIDER_GOOGLE}
-        // showsUserLocation
-        initialRegion={{
-          latitude: 33.5651,
-          longitude: 73.0169,
-          latitudeDelta: 0.09,
-          longitudeDelta: 0.05,
-        }}
-      />
+      {location?.latitude && (
+        <MapView style={{flex: 1}} showsUserLocation initialRegion={location}>
+          <Marker
+            draggable
+            coordinate={location}
+            title="You"
+            description="hehehe"
+            onDragEnd={handleMarkerChange}
+            // onPress={handleMarkerChange}
+          />
+        </MapView>
+      )}
     </View>
   );
 };
