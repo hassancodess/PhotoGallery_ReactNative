b95292d6b2dc22ee1a5dcb0e17c9c66b4cdc5bdb
hassancodess <dev.hassancodes@gmail.com>
2023-05-02 23:36:28 +0500

    integrate AlbumsContainer Component & some cleanup

--- src/screens/AlbumsTabs/Date.js
+++ src/screens/AlbumsTabs/Date.js
@@ -2,6 +2,7 @@ import React, {useState, useLayoutEffect} from 'react';
 import {StyleSheet, Text, View, FlatList, Pressable} from 'react-native';
 import {useIsFocused} from '@react-navigation/native';
 import FastImage from 'react-native-fast-image';
+import AlbumsContainer from '../../components/HomeTabs/AlbumsContainer';
 import {
   getDistinctDates,
   createAlbum,
@@ -82,38 +83,9 @@ const Date = ({navigation}) => {
     // return formattedDate;
   };
 
-  const renderItem = ({item, index}) => {
-    return (
-      <Pressable
-        onPress={() =>
-          navigation.navigate('AlbumTabs', {
-            screen: 'All',
-            params: {album: item},
-          })
-        }>
-        <View style={styles.albumContainer}>
-          <FastImage
-            style={styles.albumCover}
-            source={{
-              uri: item.cover_photo,
-              priority: FastImage.priority.normal,
-            }}
-            resizeMode={FastImage.resizeMode.cover}
-          />
-          <Text style={styles.albumContainerText}>{item.title}</Text>
-        </View>
-      </Pressable>
-    );
-  };
   return (
     <View style={styles.container}>
-      <FlatList
-        data={albums}
-        keyExtractor={(item, index) => index}
-        renderItem={renderItem}
-        numColumns={3}
-        columnWrapperStyle={{justifyContent: 'space-between'}}
-      />
+      <AlbumsContainer albums={albums} />
     </View>
   );
 };
@@ -123,7 +95,7 @@ export default Date;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingHorizontal: 20,
+    paddingHorizontal: 10,
     paddingVertical: 15,
   },
   albumContainer: {
