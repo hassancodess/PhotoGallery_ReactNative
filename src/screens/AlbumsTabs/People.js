13202811862088d835aeb9cbdf2f594ec107ab81
hassancodess <dev.hassancodes@gmail.com>
2023-05-02 23:20:26 +0500

    add AlbumsContainer Component

--- src/screens/AlbumsTabs/People.js
+++ src/screens/AlbumsTabs/People.js
@@ -1,8 +1,16 @@
-import {StyleSheet, Text, View, FlatList, Pressable} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Dimensions,
+  Pressable,
+} from 'react-native';
 import React, {useState, useLayoutEffect} from 'react';
 import FastImage from 'react-native-fast-image';
 import {useIsFocused} from '@react-navigation/native';
 import {handlePeopleAlbums} from '../../database/utils';
+import AlbumsContainer from '../../components/HomeTabs/AlbumsContainer';
 
 const People = ({navigation}) => {
   const isFocused = useIsFocused();
@@ -16,42 +24,9 @@ const People = ({navigation}) => {
     InitialSetup();
   }, [isFocused]);
 
-  const renderItem = ({item, index}) => {
-    // console.log('ITEM', item);
-    return (
-      <Pressable
-        onPress={() =>
-          navigation.navigate('AlbumTabs', {
-            screen: 'All',
-            params: {
-              album: item,
-            },
-          })
-        }>
-        <View style={styles.albumContainer}>
-          <FastImage
-            style={styles.albumCover}
-            source={{
-              uri: item.cover_photo,
-              priority: FastImage.priority.normal,
-            }}
-            resizeMode={FastImage.resizeMode.cover}
-          />
-          <Text style={styles.albumContainerText}>{item.title}</Text>
-        </View>
-      </Pressable>
-    );
-  };
-
   return (
     <View style={styles.container}>
-      <FlatList
-        data={albums}
-        keyExtractor={(item, index) => index}
-        renderItem={renderItem}
-        numColumns={3}
-        columnWrapperStyle={{justifyContent: 'space-between'}}
-      />
+      <AlbumsContainer albums={albums} />
     </View>
   );
 };
@@ -61,23 +36,6 @@ export default People;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingHorizontal: 20,
     paddingVertical: 15,
-  },
-  albumContainer: {
-    width: 100,
-    marginBottom: 20,
-    alignItems: 'center',
-  },
-  albumCover: {
-    width: 100,
-    height: 100,
-    borderRadius: 10,
-    marginBottom: 8,
-  },
-  albumContainerText: {
-    fontSize: 14,
-    color: 'black',
-    textAlign: 'center',
   },
 });
