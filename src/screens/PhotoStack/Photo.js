f74b60c7349770913b30c7e4b924d8ef36f2b885
hassancodess <dev.hassancodes@gmail.com>
2023-05-17 23:58:19 +0500

    fix photo path

--- src/screens/PhotoStack/Photo.js
+++ src/screens/PhotoStack/Photo.js
@@ -1,8 +1,6 @@
 import React, {useLayoutEffect, useState} from 'react';
 import {StyleSheet, Text, View, FlatList, Pressable} from 'react-native';
 import FastImage from 'react-native-fast-image';
-import MediaMeta from 'react-native-media-meta';
-import Exif from 'react-native-exif';
 
 const Photo = ({navigation, route}) => {
   const {photo} = route.params;
@@ -13,12 +11,13 @@ const Photo = ({navigation, route}) => {
       title: photoName,
     });
   }, []);
+
   return (
     <View style={styles.container}>
       <FastImage
         style={styles.photoCover}
         source={{
-          uri: photo.path,
+          uri: 'file://' + photo.path,
           priority: FastImage.priority.normal,
         }}
         resizeMode={FastImage.resizeMode.contain}
