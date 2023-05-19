aab9156f072df2322baf5265988825d4afc7b37f
hassancodess <dev.hassancodes@gmail.com>
2023-05-19 23:53:18 +0500

    add Avatar component

---
+++ src/components/UI/Avatar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {Avatar} from 'react-native-paper';
+import {BASE_URI} from '../../utils/api';
+
+const AvatarCircle = ({photoName, photoType, personName}) => {
+  const photoURL = `${BASE_URI}/faces/${photoName}/${personName}.${photoType}`;
+  // console.log('photourl ', photoName, photoType, personName);
+  console.log('photourl ', photoURL);
+  return (
+    <View style={styles.avatarContainer}>
+      <Avatar.Image
+        size={80}
+        source={{
+          uri: photoURL,
+        }}
+      />
+      <Text style={styles.avatarText}>{personName}</Text>
+    </View>
+  );
+};
+
+export default AvatarCircle;
+
+const styles = StyleSheet.create({
+  avatarContainer: {
+    alignItems: 'center',
+    gap: 4,
+  },
+  avatarText: {
+    color: 'black',
+    fontSize: 14,
+    fontWeight: '500',
+    width: '75%',
+    textAlign: 'center',
+  },
+});
