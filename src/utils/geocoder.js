92da2be3997fdc7d64ffedc8ce7673fbdffdcab5
hassancodess <dev.hassancodes@gmail.com>
2023-05-25 00:58:03 +0500

    improve getCity utility

--- src/utils/geocoder.js
+++ src/utils/geocoder.js
@@ -2,14 +2,18 @@ import {GMAPS_API_KEY} from './api';
 import Geocoder from 'react-native-geocoding';
 
 export const getCity = async (lat, lng) => {
-  Geocoder.init(GMAPS_API_KEY);
-  const res = await Geocoder.from(lat, lng);
-  const data = res.results[0].address_components;
-  if (data.length >= 10) {
-    const city = data[5].short_name;
-    return city;
-  } else {
-    const city = data[2].short_name;
-    return city;
+  try {
+    Geocoder.init(GMAPS_API_KEY);
+    const res = await Geocoder.from(lat, lng);
+    const data = res.results[0].address_components;
+    const cityObject = data.find(d => {
+      if (d.types.includes('locality')) {
+        return d;
+      }
+    });
+    // console.log('city', cityObject);
+    return cityObject.long_name;
+  } catch (error) {
+    console.log('error: getCity', error);
   }
 };
