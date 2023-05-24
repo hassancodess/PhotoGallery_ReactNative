55bc0c71e85d10ed23811fd17284cfc1ea396522
hassancodess <dev.hassancodes@gmail.com>
2023-05-25 01:00:12 +0500

    improve captureImage utility

--- src/utils/imagePicker.js
+++ src/utils/imagePicker.js
@@ -6,6 +6,8 @@
 import React, {useState} from 'react';
 import {PermissionsAndroid, ToastAndroid} from 'react-native';
 import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+import {showToast} from './toast';
+import {err} from 'react-native-svg/lib/typescript/xml';
 
 const requestCameraPermission = async () => {
   if (Platform.OS === 'android') {
@@ -48,56 +50,81 @@ const requestExternalWritePermission = async () => {
   }
 };
 
-export const captureImage = async type => {
+export const captureImage = async () => {
   let options = {
-    mediaType: type,
-    maxWidth: 800,
-    maxHeight: 500,
+    mediaType: 'photo',
     quality: 1,
-    videoQuality: 'low',
-    durationLimit: 30, //Video max duration in seconds
-    saveToPhotos: true,
+    includeExtra: true,
   };
   let isCameraPermitted = await requestCameraPermission();
   let isStoragePermitted = await requestExternalWritePermission();
   if (isCameraPermitted && isStoragePermitted) {
     console.log('Capture Image if block');
-    launchCamera(options, response => {
-      console.log('Response = ', response);
-
+    const imageData = await launchCamera(options, response => {
       if (response.didCancel) {
-        ToastAndroid.show('User cancelled camera picker', ToastAndroid.SHORT);
+        showToast('User cancelled camera picker', 'error');
         return;
       } else if (response.errorCode == 'camera_unavailable') {
-        ToastAndroid.show('Camera not available on device', ToastAndroid.SHORT);
+        showToast('Camera not available on device', 'error');
         return;
       } else if (response.errorCode == 'permission') {
-        ToastAndroid.show('Permission not satisfied', ToastAndroid.SHORT);
+        showToast('Permission not satisfied', 'error');
         return;
       } else if (response.errorCode == 'others') {
-        ToastAndroid.show(response.errorMessage, ToastAndroid.SHORT);
+        showToast('response.errorMessage', 'error');
         return;
       }
-      console.log('uri -> ', response.assets[0].uri);
-      console.log('width -> ', response.assets[0].width);
-      console.log('height -> ', response.height);
-      console.log('fileSize -> ', response.assets[0].fileSize);
-      console.log('type -> ', response.assets[0].type);
-      console.log('fileName -> ', response.assets[0].fileName);
-      // setImageData({
-      //   uri: response.assets[0].uri,
-      //   name: response.assets[0].fileName,
-      //   type: response.assets[0].type,
-      // });
-      const imageData = {
-        uri: response.assets[0].uri,
-        name: response.assets[0].fileName,
-        type: response.assets[0].type,
-      };
-      return imageData;
+      return response;
     });
+    return imageData.assets[0];
   }
 };
+
+// export const captureImage = async () => {
+//   let options = {
+//     mediaType: 'photo',
+//     quality: 1,
+//     includeExtra: true,
+//   };
+//   let isCameraPermitted = await requestCameraPermission();
+//   let isStoragePermitted = await requestExternalWritePermission();
+//   if (isCameraPermitted && isStoragePermitted) {
+//     await launchCamera(options, async response => {
+//       if (response.didCancel) {
+//         ToastAndroid.show('User cancelled camera picker', ToastAndroid.SHORT);
+//         return;
+//       } else if (response.errorCode == 'camera_unavailable') {
+//         ToastAndroid.show('Camera not available on device', ToastAndroid.SHORT);
+//         return;
+//       } else if (response.errorCode == 'permission') {
+//         ToastAndroid.show('Permission not satisfied', ToastAndroid.SHORT);
+//         return;
+//       } else if (response.errorCode == 'others') {
+//         ToastAndroid.show(response.errorMessage, ToastAndroid.SHORT);
+//         return;
+//       }
+//       // console.log('uri -> ', response.assets[0].uri);
+//       // console.log('width -> ', response.assets[0].width);
+//       // console.log('height -> ', response.height);
+//       // console.log('fileSize -> ', response.assets[0].fileSize);
+//       // console.log('type -> ', response.assets[0].type);
+//       // console.log('fileName -> ', response.assets[0].fileName);
+//       // console.log(response);
+//       // setImageData({
+//       //   uri: response.assets[0].uri,
+//       //   name: response.assets[0].fileName,
+//       //   type: response.assets[0].type,
+//       // });
+//       // const imageData = {
+//       //   uri: response.assets[0].uri,
+//       //   name: response.assets[0].fileName,
+//       //   type: response.assets[0].type,
+//       // };
+//       const data = await response.assets;
+//       return data;
+//     });
+//   }
+// };
 
 export const chooseFile = type => {
   let options = {
