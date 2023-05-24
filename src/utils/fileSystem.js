da8da0dac34838562e923708cbb12c730fd45c66
hassancodess <dev.hassancodes@gmail.com>
2023-05-25 00:57:21 +0500

    add store image utility

--- src/utils/fileSystem.js
+++ src/utils/fileSystem.js
@@ -1,4 +1,5 @@
 import RNFS from 'react-native-fs';
+import {showToast} from './toast';
 
 const folderPath = RNFS.PicturesDirectoryPath + '/PhotoGallery/';
 
@@ -10,4 +11,18 @@ export const getAllImages = async () => {
 export const getSomeImages = async count => {
   const response = await RNFS.readDir(folderPath);
   return response.slice(0, count);
+};
+
+export const storeImage = async photo => {
+  const destinationPath = `${folderPath}${photo.name}`;
+  console.log('destinationPath', destinationPath);
+  RNFS.moveFile(photo.uri, destinationPath)
+    .then(result => {
+      console.log('Successfully moved file to specified destination.');
+      showToast('Successfully moved file to specified destination.');
+    })
+    .catch(err => {
+      console.log(err.message);
+      showToast(err.message, 'error');
+    });
 };
