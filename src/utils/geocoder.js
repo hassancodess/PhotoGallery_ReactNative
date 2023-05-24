41ae3e727a94dda666f953724922b53dc35483ea
hassancodess <dev.hassancodes@gmail.com>
2023-05-24 08:12:16 +0500

    cleanup

--- src/utils/geocoder.js
+++ src/utils/geocoder.js
@@ -5,7 +5,6 @@ export const getCity = async (lat, lng) => {
   Geocoder.init(GMAPS_API_KEY);
   const res = await Geocoder.from(lat, lng);
   const data = res.results[0].address_components;
-  console.log('address', data);
   if (data.length >= 10) {
     const city = data[5].short_name;
     return city;
