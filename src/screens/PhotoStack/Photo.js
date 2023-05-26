4845f6d5178c290963a8a93bd0fc05c3ba50ac0d
hassancodess <dev.hassancodes@gmail.com>
2023-05-26 14:25:50 +0500

    add API calls

--- src/screens/PhotoStack/Photo.js
+++ src/screens/PhotoStack/Photo.js
@@ -8,6 +8,8 @@ import {
   Dimensions,
 } from 'react-native';
 import FastImage from 'react-native-fast-image';
+import {BASE_URI} from '../../utils/api';
+import {addPeopleToDatabase, handlePersons} from '../../database/helpers';
 
 const Photo = ({navigation, route}) => {
   const {photo} = route.params;
@@ -16,7 +18,51 @@ const Photo = ({navigation, route}) => {
     navigation.setOptions({
       title: photoName,
     });
+    // callAPI();
+    init();
   }, []);
+
+  const init = async () => {
+    await handlePersons(photo);
+  };
+  const callAPI = async () => {
+    try {
+      const photoType = photo.title.split('.').pop();
+      // console.log('photo type', photoType, 'file://'+photo.path, photo.title);
+      console.log('started');
+      const formdata = new FormData();
+      formdata.append('file', {
+        uri: 'file://' + photo.path,
+        name: photo.title,
+        type: `image/${photoType}`,
+      });
+      var requestOptions = {
+        method: 'POST',
+        body: formdata,
+        redirect: 'follow',
+      };
+
+      const response = await fetch(`${BASE_URI}/saveImage`, requestOptions);
+
+      const data = await response.json();
+      console.log('data', data);
+
+      const dataPeople = [];
+      if (typeof data !== 'string') {
+        for (const key in data) {
+          const data = {
+            id: `uuid-${Math.floor(Math.random() * 100) + 1}`,
+            name: key,
+          };
+          dataPeople.push(data);
+        }
+      }
+      // console.log('Data People', dataPeople);
+      // await addPeopleToDatabase(dataPeople, photo.id);
+    } catch (error) {
+      console.log('Error', error);
+    }
+  };
 
   return (
     <View style={styles.container}>
