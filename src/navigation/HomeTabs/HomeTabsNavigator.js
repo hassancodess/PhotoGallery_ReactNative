00d29f95935a4d0c59f7cd3613c9b5a6580db1bb
hassancodess <dev.hassancodes@gmail.com>
2023-05-27 14:49:14 +0500

    bug fix

--- src/navigation/HomeTabs/HomeTabsNavigator.js
+++ src/navigation/HomeTabs/HomeTabsNavigator.js
@@ -11,6 +11,8 @@ import Search from '../../screens/HomeTabs/Search';
 import Camera from '../../screens/HomeTabs/Camera';
 // Navigator
 const {Navigator, Screen} = createBottomTabNavigator();
+// Context
+import {PhotoContextProvider} from '../../context/PhotoContext';
 // Icons
 import Icon from 'react-native-vector-icons/FontAwesome';
 import GlobalStyles from '../../utils/GlobalStyles';
@@ -25,72 +27,78 @@ const icons = {
 
 const HomeTabsNavigator = () => {
   return (
-    <Navigator
-      initialRouteName="AlbumsStack"
-      screenOptions={({route}) => ({
-        headerShown: false,
-        tabBarShowLabel: false,
-        tabBarIcon: ({color, size}) => {
-          return (
-            <View
-              style={{justifyContent: 'center', alignItems: 'center', gap: 6}}>
-              <Icon name={icons[route.name]} size={size} color={color} />
-              <Text style={{color: GlobalStyles.colors.white, fontSize: 12}}>
-                {route.name === 'AlbumsStack' ? 'Albums' : route.name}
-              </Text>
-            </View>
-          );
-        },
-        tabBarActiveTintColor: GlobalStyles.colors.white,
-        tabBarActiveBackgroundColor: GlobalStyles.colors.primaryActive,
+    <PhotoContextProvider>
+      <Navigator
+        initialRouteName="AlbumsStack"
+        screenOptions={({route}) => ({
+          headerShown: false,
+          tabBarShowLabel: false,
+          tabBarIcon: ({color, size}) => {
+            return (
+              <View
+                style={{
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  gap: 6,
+                }}>
+                <Icon name={icons[route.name]} size={size} color={color} />
+                <Text style={{color: GlobalStyles.colors.white, fontSize: 12}}>
+                  {route.name === 'AlbumsStack' ? 'Albums' : route.name}
+                </Text>
+              </View>
+            );
+          },
+          tabBarActiveTintColor: GlobalStyles.colors.white,
+          tabBarActiveBackgroundColor: GlobalStyles.colors.primaryActive,
 
-        tabBarInactiveBackgroundColor: GlobalStyles.colors.primary,
-        tabBarInactiveTintColor: GlobalStyles.colors.screen,
-      })}>
-      <Screen
-        name="AlbumsStack"
-        component={AlbumsStackNavigator}
-        options={({route}) => ({
-          // lazy: true,
-          title: 'Albums',
-          tabBarStyle: (route => {
-            const routeName = getFocusedRouteNameFromRoute(route) ?? '';
-            if (routeName === 'Album' || routeName === 'PhotoStack') {
-              return {display: 'none'};
-            }
-            return styles.tabBar;
-          })(route),
-        })}
-      />
-      <Screen
-        name="Search"
-        component={Search}
-        options={{
-          tabBarStyle: styles.tabBar,
-        }}
-      />
-      <Screen
-        name="Camera"
-        component={Camera}
-        options={{
-          tabBarStyle: styles.tabBar,
-        }}
-      />
-      <Screen
-        name="Map"
-        component={Map}
-        options={{
-          tabBarStyle: styles.tabBar,
-        }}
-      />
-      <Screen
-        name="Settings"
-        component={Settings}
-        options={{
-          tabBarStyle: styles.tabBar,
-        }}
-      />
-    </Navigator>
+          tabBarInactiveBackgroundColor: GlobalStyles.colors.primary,
+          tabBarInactiveTintColor: GlobalStyles.colors.screen,
+        })}>
+        <Screen
+          name="AlbumsStack"
+          component={AlbumsStackNavigator}
+          options={({route}) => ({
+            // lazy: true,
+            title: 'Albums',
+            tabBarStyle: (route => {
+              const routeName = getFocusedRouteNameFromRoute(route) ?? '';
+              if (routeName === 'Album' || routeName === 'PhotoStack') {
+                return {display: 'none'};
+              }
+              return styles.tabBar;
+            })(route),
+          })}
+        />
+        <Screen
+          name="Search"
+          component={Search}
+          options={{
+            tabBarStyle: styles.tabBar,
+          }}
+        />
+        <Screen
+          name="Camera"
+          component={Camera}
+          options={{
+            tabBarStyle: styles.tabBar,
+          }}
+        />
+        <Screen
+          name="Map"
+          component={Map}
+          options={{
+            tabBarStyle: styles.tabBar,
+          }}
+        />
+        <Screen
+          name="Settings"
+          component={Settings}
+          options={{
+            tabBarStyle: styles.tabBar,
+          }}
+        />
+      </Navigator>
+    </PhotoContextProvider>
   );
 };
 
