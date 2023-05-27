7a98da6ae90f7b9d609b27cf401e7f955748f52a
hassancodess <dev.hassancodes@gmail.com>
2023-05-27 14:47:41 +0500

    create context

---
+++ src/context/PhotoContext.js
@@ -0,0 +1,88 @@
+import React, {useState, createContext, useEffect} from 'react';
+import {getAllEventsOfPhoto, getPeopleInPhoto} from '../database/helpers';
+
+export const PhotoContext = createContext(null);
+
+export const PhotoContextProvider = ({children}) => {
+  const [photo, setPhoto] = useState();
+  const [photoType, setPhotoType] = useState();
+  const [photoName, setPhotoName] = useState();
+  const [people, setPeople] = useState([]);
+  const [events, setEvents] = useState([]);
+  const [person, setPerson] = useState({});
+  const [isPersonTextDisabled, setIsPersonTextDisabled] = useState(true);
+
+  useEffect(() => {
+    if (photo) {
+      // SET Photo Type & Name
+      const name = photo.path.split('/').pop().split('.')[0];
+      const type = photo.title.split('.').pop();
+      setPhotoName(name);
+      setPhotoType(type);
+      // RESET Person States
+      resetPersonStates();
+      // SET Photo People & Events
+      fetchPeopleAndEvents();
+    }
+  }, [photo]);
+
+  // useEffect(() => {
+  //   if (photo) {
+  //     handlePeopleAndEvents();
+  //   }
+  // }, [events]);
+  // useEffect(() => {
+  //   if (photo) {
+  //     handlePeopleAndEvents();
+  //   }
+  // }, [people]);
+
+  // useEffect(() => {
+  //   if (people || events) {
+  //     console.log('People', people);
+  //     console.log('Events', events);
+  //   }
+  // }, [events, people]);
+
+  const fetchPeopleAndEvents = async () => {
+    const eventsList = await getAllEventsOfPhoto(photo.id);
+    const personsList = await getPeopleInPhoto(photo.id);
+
+    console.log('People', personsList);
+    console.log('Events', eventsList);
+
+    setPeople(personsList);
+    setEvents(eventsList);
+  };
+
+  const resetPersonStates = async () => {
+    setPerson({});
+    setIsPersonTextDisabled(true);
+  };
+  const setPersonStates = async person => {
+    setIsPersonTextDisabled(false);
+    setPerson({id: person.id, name: person.name});
+  };
+
+  const values = {
+    photo,
+    person,
+    isPersonTextDisabled,
+    photoName,
+    photoType,
+    people,
+    events,
+    setPhoto,
+    setPerson,
+    setIsPersonTextDisabled,
+    setPeople,
+    setEvents,
+    resetPersonStates,
+    fetchPeopleAndEvents,
+    setPersonStates,
+  };
+  return (
+    <PhotoContext.Provider value={values}>{children}</PhotoContext.Provider>
+  );
+};
+export default PhotoContext;
