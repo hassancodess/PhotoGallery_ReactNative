b5c7163d2935e9b38526de1c179c75a44d0fec16
hassancodess <dev.hassancodes@gmail.com>
2023-05-29 15:19:19 +0500

    minor change

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,2 +1,3 @@
-export const BASE_URI = 'http://192.168.43.145:81';
+// export const BASE_URI = 'http://192.168.43.145:81';
+export const BASE_URI = 'http://192.168.100.80:81/';
 export const GMAPS_API_KEY = 'AIzaSyDy_6oDRGGLua2ins00ATc_CMarYXEJTzk';
