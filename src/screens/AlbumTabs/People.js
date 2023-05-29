36dec2947f1b6445f72452560e01c33205b9d311
hassancodess <dev.hassancodes@gmail.com>
2023-05-29 16:13:38 +0500

    handle nested people

--- src/screens/AlbumTabs/People.js
+++ src/screens/AlbumTabs/People.js
@@ -1,10 +1,37 @@
+import React, {useState, useEffect, useLayoutEffect} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {useIsFocused, useNavigation, useRoute} from '@react-navigation/native';
+import {initialEventsSetup, initialPeopleSetup} from '../../database/helpers';
+import AlbumsContainer from '../../components/HomeTabs/AlbumsContainer';
+import PhotoContainer from '../../components/AlbumTabs/PhotoContainer';
 
 const People = () => {
+  // Navigation
+  const navigation = useNavigation();
+  const isFocused = useIsFocused();
+  const route = useRoute();
+  const {album} = route.params;
+  // console.log('pics', album.photos);
+  // States
+  const [albums, setAlbums] = useState([]);
+
+  // Utilities
+  const init = async () => {
+    const albumsByPeople = await initialPeopleSetup(album);
+    setAlbums(albumsByPeople);
+  };
+  useLayoutEffect(() => {
+    init();
+  }, []);
+
+  useEffect(() => {
+    console.log('albums', albums);
+  }, [albums]);
+
   return (
-    <View>
-      <Text>People</Text>
+    <View style={styles.container}>
+      {albums.length === 1 && <PhotoContainer photos={albums[0].photos} />}
+      {albums.length > 1 && <AlbumsContainer albums={albums} />}
     </View>
   );
 };
